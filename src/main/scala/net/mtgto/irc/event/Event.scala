24d58ccf8e86d2aef370d8a19adb44438a6528fb
mtgto <hogerappa@gmail.com>
2012-09-17 21:56:08 +0900

    collect event subclasses.

--- src/main/scala/net/mtgto/irc/event/Event.scala
+++ src/main/scala/net/mtgto/irc/event/Event.scala
@@ -2,6 +2,29 @@ package net.mtgto.irc.event
 
 import java.util.Date
 
-trait Event {
+sealed trait Event {
   val date = new Date
 }
+
+/**
+ * a message someone sent to a channel.
+ */
+case class Message(
+  channel: String,
+  nickname: String,
+  username: String,
+  hostname: String,
+  text: String,
+  override val date: Date
+) extends Event
+
+/**
+ * a private message someone sent to the irc bot.
+ */
+case class PrivateMessage(
+  nickname: String,
+  username: String,
+  hostname: String,
+  text: String,
+  override val date: Date
+) extends Event
